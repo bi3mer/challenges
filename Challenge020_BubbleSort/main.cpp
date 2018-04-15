d218b8430f9ecb8839e4ee080c9baa9aa40da576
Colan Biemer <colan@comcast.net>
2018-04-14 20:35:42 -0700

    bubble sort visualization

--- Challenge020_BubbleSort/main.cpp
+++ Challenge020_BubbleSort/main.cpp
@@ -1,44 +1,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <algorithm> 
+#include "ArrayVisualizer.cpp"
 
-void printArray(int* arr, int size)
-{
-	for(int i = 0; i < size; ++i)
-	{
-		printf("%i", arr[i]);
-
-		if(i < size - 1)
-		{
-			printf(",");
-		}
-	}
-
-	printf("\n");
-}
-
-
-void naiveBubbleSort(int* arr, int size)
-{
-	bool sorted = false;
-
-	while(sorted == false)
-	{
-		bool swapOccurred = false;
-		for(int i = 0; i < size - 1; ++i)
-		{
-			if(arr[i] > arr[i + 1])
-			{
-				std::swap(arr[i], arr[i+1]);
-				swapOccurred = true;
-			}
-		}		
-
-		sorted = !swapOccurred;
-	}
-}
-
-void bubbleSort(int* arr, int size)
+void bubbleSort(int* a, int size)
 {
 	bool swapped;
 
@@ -48,10 +13,10 @@ void bubbleSort(int* arr, int size)
 
 		for(int j = 0; j < size - i - 1; ++j)
 		{
-			if(arr[j] > arr[j+1])
+			if(a[j] > a[j+1])
 			{
 				swapped = true;
-				std::swap(arr[j], arr[j+1]);
+				swap(j, j+1);
 			}
 		}
 
@@ -62,26 +27,11 @@ void bubbleSort(int* arr, int size)
 	}
 }
 
-int main(int argc, char* argv[]) {
-	int a[] = {0,-1,3,10,100,3};
-	int b[] = {-1};
-	int c[] = {0,0,0,1235,-1};
-	int d[] = {};
-
-	int len_a = 6;
-	int len_b = 1;
-	int len_c = 5;
-	int len_d = 0;
-
-	bubbleSort(a, len_a);
-	bubbleSort(b, len_b);
-	bubbleSort(c, len_c);
-	bubbleSort(d, len_d);
-
-	printArray(a, len_a);
-	printArray(b, len_b);
-	printArray(c, len_c);
-	printArray(d, len_d);
+int main(int argc, char* argv[]) 
+{
+	length = 500;
+	setUpGlutAndArray(argc, argv, bubbleSort);
+	free(arr);
 
 	return 0;
 }
--- SortVisualizer/ArrayVisualizer.cpp
+++ SortVisualizer/ArrayVisualizer.cpp
@@ -6,11 +6,13 @@
 #include <algorithm>
 #include <iostream>
 #include "ArrayHelper.cpp"
+#include <unistd.h>
 
 // I know this is bad, but oh well :/ Not going for best design, just
 // trying to learn
 int length;
 int* arr;
+void (*sort)(int*, int);
 
 // used https://www.codeproject.com/Articles/182109/Setting-up-an-OpenGL-development-environment-in-Ub
 // as starting place
@@ -62,23 +64,31 @@ void renderFunction()
 void swap(int index1, int index2)
 {
 	std::swap(arr[index1], arr[index2]);
-	glutPostRedisplay();
+	renderFunction();
+	usleep(150);
 }
 
-// exit on escape key pressed
 void keyboardEvent(unsigned char c, int x, int y)
 {
 	if(c == 27)
 	{
+		// exit on escape key pressed
 		exit(0);
+		free(arr);
+	}
+	else if(c == 115)
+	{
+		// start on `s` key pressed
+		sort(arr, length);
 	}
 }
 
 /* Main method - main entry point of application
 the freeglut library does the window creation work for us, 
 regardless of the platform. */
-int setUpGlutAndArray(int argc, char** argv)
+int setUpGlutAndArray(int argc, char** argv, void (*sortingAlgorithm)(int*, int))
 {
+	sort = sortingAlgorithm;
 	arr = (int*) malloc(sizeof(int) * length);
 	for(int i = 0; i < length; ++i)
 	{
@@ -96,9 +106,10 @@ int setUpGlutAndArray(int argc, char** argv)
 	glutDisplayFunc(renderFunction);
 	glutKeyboardFunc(keyboardEvent);
 
-	glutMainLoop();	
+	glutMainLoop();
 }
 
+/* testing code
 int main(int argc, char** argv)
 {
 	length = 400;
@@ -106,3 +117,4 @@ int main(int argc, char** argv)
 	free(arr);
 	return 0;
 }
+*/
